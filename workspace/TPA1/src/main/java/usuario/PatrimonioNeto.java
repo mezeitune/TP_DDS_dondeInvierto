f3cee85ec595ff8c86eaa19f35c9a0c8de8ac9e4
mezeitune <matizeitune@gmail.com>
2017-05-30 21:45:08 -0300

    Cambio en la forma de evaluar Predefinido y singleton

--- workspace/TPA1/src/main/java/usuario/PatrimonioNeto.java
+++ workspace/TPA1/src/main/java/usuario/PatrimonioNeto.java
@@ -7,17 +7,24 @@ import org.uqbar.commons.utils.Observable;
 public class PatrimonioNeto extends Indicador{
 
 	
-	private static PatrimonioNeto singleton = new PatrimonioNeto("PN", "1+2" );
+	private static PatrimonioNeto instance ;
+
+
 
 
 	public static PatrimonioNeto getInstance( ) {
-	    return singleton;
-	}
+        if(instance == null){
+            instance = new PatrimonioNeto("PN", "1+2" );
+        }
+        return instance;
+
+	}	
 	
 	public PatrimonioNeto(String nombre,String formula) {
 		super(nombre,formula);
 	}
 
+	
 
 
 
