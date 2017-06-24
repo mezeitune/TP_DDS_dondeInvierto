83184d5da4c1d0c910d5e6f4f681826a52b7b53b
mezeitune <matizeitune@gmail.com>
2017-06-23 21:06:47 -0300

    CondicionMixta terminada

--- workspace/TPA1/src/main/java/metodologias/CondicionMixta.java
+++ workspace/TPA1/src/main/java/metodologias/CondicionMixta.java
@@ -20,14 +20,25 @@ public class CondicionMixta implements EstadoCondicion{
 
 	}	
 	
+	
+	
 	@Override
 	public int evaluar(Empresa empresa1, Empresa empresa2, Condicion condicion) {
 		ParametroOperacion parametroOperacionTaxativas=condicion.getParametroOperacionTaxativa();
 		ParametroOperacion parametroOperacionComparativas=condicion.getParametroOperacionComparativa();
 		int year = Calendar.getInstance().get(Calendar.YEAR);
 		
+		int evaluacionTaxativa=CondicionTaxativa.getInstance().evaluar(empresa1, condicion);
+		int evaluacionComparativa=CondicionComparativa.getInstance().evaluar(empresa1, empresa2, condicion);
 		
-		return 0;
+		if(evaluacionComparativa+evaluacionTaxativa==2)//se cumplieron las dos condiciones
+		{
+			return 1;
+		}else{
+			return 0;
+		}
+		
+		
 		
 	}
 }
