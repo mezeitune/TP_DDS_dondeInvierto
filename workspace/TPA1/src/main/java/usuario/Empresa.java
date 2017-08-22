43e1628007e38756b65ae7da7f0670dd185df0f1
mezeitune <matizeitune@gmail.com>
2017-08-22 14:06:50 -0300

    probando many to many

--- workspace/TPA1/src/main/java/usuario/Empresa.java
+++ workspace/TPA1/src/main/java/usuario/Empresa.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
