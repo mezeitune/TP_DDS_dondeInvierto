d71a3e9438b8dea2951d3fee86eba31ac6dfb360
mezeitune <matizeitune@gmail.com>
2017-08-22 11:31:04 -0300

    Test inicial persistencia

---
+++ workspace/TPA1/src/test/java/PersistenciaRelacionalTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
+
+public class PersistenciaRelacionalTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
+
+
+		
+		@Test
+		public void contextUp() {
+			assertNotNull(entityManager());
+		}
+
+		@Test
+		public void contextUpWithTransaction() throws Exception {
+			withTransaction(() -> {});
+		}
+		
+		
+
+
+	
+}
