1f1fd292dad8e01bbb9a9b778bf425bab5206e22
mezeitune <matizeitune@gmail.com>
2017-10-08 20:04:10 -0300

    formulario login agregado estatico

---
+++ workspace/TPA1/src/main/java/usuario/PersistentObject.java
@@ -0,0 +1,13 @@
+package usuario;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class PersistentObject {
+
+	@Id @GeneratedValue
+	private Long id;
+}
