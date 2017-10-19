4419c8a7f53b74b2bc254c69d1f6004b2fc41820
mezeitune <matizeitune@gmail.com>
2017-10-19 01:13:32 -0300

    fix 2

--- workspace/TPA1/src/main/java/Server/Loader.java
+++ workspace/TPA1/src/main/java/Server/Loader.java
@@ -26,15 +26,12 @@ public class Loader {
 		List<Indicador> indicadores = new IndicadoresMock().getIndicadoresMockeados();
 		
 		Usuario root = new Usuario("root","root");
-		Usuario admin = new Usuario("admin","admin");
 		
 		root.setIndicadores(indicadores);
-		admin.setIndicadores(indicadores);
 		
 		List<Usuario> usuarios = new LinkedList<Usuario>();
 		
 		usuarios.add(root);
-		//usuarios.add(admin); TODO: Problema, los indicadores solo quedan referenciado por el admin, y no por root tambien.
 		
 		repositorio_empresas.begin();
 		empresas.forEach(empresa -> repositorio_empresas.agregar(empresa));
