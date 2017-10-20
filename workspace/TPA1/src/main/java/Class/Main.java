e276e7cfea4481e1d94376fd451311ee5021ae1f
mdasilvaevora <manudse10@gmail.com>
2017-10-20 17:02:48 -0300

    Limpiando persistence.xml

--- workspace/TPA1/src/main/java/Class/Main.java
+++ workspace/TPA1/src/main/java/Class/Main.java
@@ -1,31 +1,17 @@
 package Class;
 
 
-import org.uqbar.arena.Application;
-import org.uqbar.arena.windows.Window;
 import org.uqbar.commons.utils.Observable;
 
-import excepciones.CSVInexistenteException;
-import ui.windows.MenuWindow;
 
 @Observable
-public class Main extends Application{
+public class Main{
 
 	
 	public static void main(String[] args){
-		new Main().start();
+		
 	}
-		@Override
-	protected Window<?> createMainWindow() {
-
-			try {
-				return new MenuWindow(this);
-			} catch (CSVInexistenteException ex) {
-				ex.printStackTrace();
-			}
-			return null;
-
-	}
+	
 		
 	}
 
