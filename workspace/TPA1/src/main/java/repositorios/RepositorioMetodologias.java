ef8f284289324cd830667f749599b8258e30e840
Manuel da Silva Evora <manudse10@gmail.com>
2017-11-08 20:56:39 -0300

    Fix repositorios + gitignore actualizado

--- workspace/TPA1/src/main/java/repositorios/RepositorioMetodologias.java
+++ workspace/TPA1/src/main/java/repositorios/RepositorioMetodologias.java
@@ -1,21 +1,19 @@
 package repositorios;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.persistence.Query;
 
 
-import metodologiasPredefinidas.WarrenBuffet;
 import model.Metodologia;
 
 public class RepositorioMetodologias extends RepositorioDBRelational<Metodologia> {
 
+	@SuppressWarnings("unchecked")
 	public  List<Metodologia> getMetodologias(){
-		List<Metodologia> metodologias = new LinkedList<Metodologia> ();
-		this.cargarMetodologias(metodologias);
-		return metodologias;
+		Query queryIndicadores = entityManager().createQuery("from Metodologia");
+		return queryIndicadores.getResultList();
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -23,18 +21,7 @@ public class RepositorioMetodologias extends RepositorioDBRelational<Metodologia
 		Query queryIndicadores = entityManager().createQuery("from Metodologia");
 		return queryIndicadores.getResultList(); 
 	}
-	
-	
-	public  void cargarMetodologias(List<Metodologia> metodologias) {
-		this.getMetodologiasDefinidasPorElUsuario().stream().forEach(metodologiaDefinidaPorUsuario -> metodologias.add(metodologiaDefinidaPorUsuario));
-		this.getMetodologiasPredefinidas().stream().forEach(metodologiaPredefinida -> metodologias.add(metodologiaPredefinida));
-	}
 
-	public  List<Metodologia> getMetodologiasPredefinidas(){
-		List<Metodologia> metodologiasPredefinidas = new LinkedList<Metodologia>();
-		metodologiasPredefinidas.add(WarrenBuffet.getInstance());
-		return metodologiasPredefinidas;
-	}
 
 	public Metodologia getMetodologia(String nombreMet){
 		return this.getMetodologias().stream().filter(unInd -> unInd.getNombre().equals(nombreMet)).collect(Collectors.toList()).get(0);
